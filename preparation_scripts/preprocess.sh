1c61dc0a758014b2357a29ebe1315193563dee56
asajadi <asajadi@gmail.com>
2016-03-29 14:53:45 -0300

    modifying preprocessing

--- preparation_scripts/preprocess.sh
+++ preparation_scripts/preprocess.sh
@@ -1,36 +1,19 @@
+#Automatic constructing databases, 
 
-#Downloading the datasets, it might take a while, and make sure the destination exists 
-wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz            \
-	-P ~/Downloads/wikidumps
-wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz      \
-	 -P ~/Downloads/wikidumps
-wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-redirect.sql.gz       \
-	 -P ~/Downloads/wikidumps
-wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-category.sql.gz       \
-	-P ~/Downloads/wikidumps
-wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-categorylinks.sql.gz \
-	-P ~/Downloads/wikidumps
+#input: $1 mysql user, $2: mysql password
 
-gunzip -c ~/Downloads/wikidumps/enwiki-latest-page.sql.gz		\
-		> ~/Downloads/wikidumps/enwiki-latest-page.sql 		
-gunzip -c ~/Downloads/wikidumps/enwiki-latest-pagelinks.sql.gz 	\
-		> ~/Downloads/wikidumps/enwiki-latest-pagelinks.sql	
-gunzip -c ~/Downloads/wikidumps/enwiki-latest-redirect.sql.gz		\
-		> ~/Downloads/wikidumps/enwiki-latest-redirect.sql	
-gunzip -c ~/Downloads/wikidumps/enwiki-latest-category.sql.gz		\
-		> ~/Downloads/wikidumps/enwiki-latest-category.sql	
-gunzip -c ~/Downloads/wikidumps/enwiki-latest-categorylinks.sql.gz	\
-		> ~/Downloads/wikidumps/enwiki-latest-categorylinks.sql	
+# Download wikipedia dumps to ~/Downloads/wikidumps, make sure it exists
+bash download.sh
 
-java ProcessSQLDumps ~/Downloads/wikidumps
+# Decompress the dumps
+bash decompress.sh
 
-#mysql -u <u> -p<p> -e 'set global key_buffer_size=4*1024*1024*1024;'
-#mysql -u <u> -p<p> -e 'set global bulk_insert_buffer_size=1024*1024*1024;'
-#mysql -u <u> -p<p> -e 'set global query_cache_size = 4*1024*1024*1024;'
-#mysql -u <u> -p<p> -e 'set global query_cache_limit = 4*1024*1024*1024;'
-#mysql -u <u> -p<p> -e 'set global tmp_table_size = 4*1024*1024*1024;'
+# Parsing the dumps
+bash parsdumps.sh
 
-#mysql -u <u> -p<p> -e 'CREATE SCHEMA `enwikilast` DEFAULT CHARACTER SET binary;'
-#./importall  ~/Downloads/wikidumps last <MYSQLROOTPASSWORD> 
+# Setting up mysql variables
+bash setupmysql.sh $1 $2
 
+# Start importing
+bash startimport.sh $1 $2
 
